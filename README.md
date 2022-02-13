09395de9e5f05275ff908cc524cbfb11240db55c
Anthony Gladden <a.gladden360@outlook.com>
2022-02-13 13:20:34 -0800

    added test to readme and fixed scripts.js

---
+++ README.md
@@ -0,0 +1,7 @@
+Describe: beepboop()
+test:beepboop(1) 
+Expected Output: 'beep'
+test:beepboop(2)
+Expected Output: 'beep','boop'
+test: beepboop(3)
+Expected Output: 'bee','boop','wont you be my neighbor?'
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
 // business logic //
 $(document).ready(function() {
   $('form#input').submit(function(event) {
+    event.preventDefault();
 
-    const input = $('input:text[name=numbers]').val();
-    const numbers = parseInt(input);
-    const arrayNumbers = []
+  const input = $('#numbers').val(); 
+  const array = ['beep', 'boop', 'wont you be my neighbor?'];
+    console.log(array['0'])
+
+  function outPutNumber(output) {  
+    return array;
+  }
+  // if input === 1 {
+  //   return array['0']
+  // } else if input === 2 {
+  //   return array['1']
+  // } else input === 3 {
+  //   return array['3']
+  // }
   
-    function outputNumber = numbers {
-      
 
-    }
-    
-    
-
-  event.preventDefault();
+  
 
   });
 });
