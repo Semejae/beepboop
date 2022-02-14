cc0243040f9df8c1709c04983b89efc43937103b
Anthony Gladden <a.gladden360@outlook.com>
2022-02-13 16:46:40 -0800

    added if else to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,27 @@ $(document).ready(function() {
     event.preventDefault();
 
   const input = $('#numbers').val(); 
+  const number = parseInt(input)
   const array = ['beep', 'boop', 'wont you be my neighbor?'];
-    console.log(array['0'])
+  
+  
+  // console.log(number)
 
-  function outPutNumber(output) {  
-    return array;
+    for (let i = 0; i <= 'beep', 'boop', 'wont you be my neighbor?'; i++) { 
+    
+    if (number === 1 ) {
+    return array['0']
+  
+    } else if (number === 2 ) {
+    return array['1']
+
+    } else (number === 3 ); {
+    return array['2']
+    }
+  
   }
-  // if input === 1 {
-  //   return array['0']
-  // } else if input === 2 {
-  //   return array['1']
-  // } else input === 3 {
-  //   return array['3']
-  // }
-  
+    
 
-  
 
   });
 });
