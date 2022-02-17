275a9bd762f17d5dba383b6ebefcd2efc89daac6
Anthony Gladden <a.gladden360@outlook.com>
2022-02-16 19:13:21 -0800

    changed scripts added ui logic and displayed results too page

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ test:beepboop(2)
 Expected Output: 'beep','boop'
 test: beepboop(3)
 Expected Output: 'bee','boop','wont you be my neighbor?'
+
+## BeepBoop
+
+### by_**{Anthony-Gladden}**_
+
+### _{this is a page that turns numbers into words}_
+
+## technologies
--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,42 @@
 // business logic //
+
+function roboBeep(inputNum){
+  const arrayNumber = []
+  for (let i = 0; i <= inputNum; i++) {
+      
+    let numberImLookingAt = i.toString();
+    let result = arrayNumber.toString();
+  
+    if (numberImLookingAt.includes('3')) {
+      arrayNumber.push('Wont you be my neighbor?')
+    } else if (numberImLookingAt.includes('2')) {
+      arrayNumber.push('boop')
+    } else if (numberImLookingAt.includes('1')) {
+      arrayNumber.push('beep')
+    } else {
+      console.log(result)
+    }
+  }
+  return arrayNumber  
+}
+
+
+
+
+
+
+// UI logic
 $(document).ready(function() {
   $('form#input').submit(function(event) {
     event.preventDefault();
 
   const input = $('#numbers').val(); 
-  const number = parseInt(input)
-  const arrayNumber = []
+  const inputNum = parseInt(input);
+  const outputArray = roboBeep(inputNum);
   
+  console.log(outputArray)
+  $('#results').text(outputArray)
   
-  // console.log(array.length)
-    
-    for (let i = 1; i <= number; i++) {
-      
-      let numberImLookingAt = i.toString();
-    
-      if (numberImLookingAt.includes('3')) {
-        arrayNumber.push('Wont you be my neighbor?')
-      
-      } else if (numberImLookingAt.includes('2')) {
-        arrayNumber.push('boop')
-      
-      } else if (numberImLookingAt.includes('1')) {
-        arrayNumber.push('beep')
-
-      } else {
-        arrayNumber.push(numberImLookingAt)
-      console.log(arrayNumber)
-      };
-    
-    }
   });
 });
+
